019eaf7d791269b391c55e0e4d01b20e81fc1760
furkandel <57002551+Furkan-del@users.noreply.github.com>
2023-03-29 16:35:44 +0300

    commentservice-commentRepo-chatgptService-Impl added.

--- src/main/java/com/example/chatbot/controller/CommentController.java
+++ src/main/java/com/example/chatbot/controller/CommentController.java
@@ -1,16 +1,54 @@
 package com.example.chatbot.controller;
 
+import com.example.chatbot.business.concrete.ChatGptServiceImpl;
 import com.example.chatbot.business.concrete.CommentServiceImpl;
+import com.example.chatbot.entity.Comment;
+import com.example.chatbot.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @Data
 @AllArgsConstructor
 public class CommentController {
-private  final CommentServiceImpl commentService;
+    private final CommentServiceImpl commentService;
+    private final ChatGptServiceImpl chatGptService;
+
+    @GetMapping("/comments")
+    public String getAllComments(Model model) {
+        model.addAttribute("commentsAll", commentService.getAllComments());
+        return "comment";
+    }
+
+    @PostMapping("/comments")
+    public String postComment(@ModelAttribute("commentsAll") Comment comment, @RequestParam("comment") String comments,Model model) {
+        comment.setComment(comments);
+        Date currentDate = new Date();
+        DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+        comment.setUserName("Furkan");
+        comment.setDates(df.format(currentDate));
+        String answer=chatGptService.generateText(comments);
+        comment.setCommentType(answer);
+
+       /* comment.setUserId(1L);*/
+        commentService.add(comment);
 
 
+        return "redirect:/comments";
+    }
+    @GetMapping("comments/delete/{id}")
+    public  String delete(@PathVariable Long id){
+        Comment comment=commentService.getCommentById(id);
+        commentService.delete(comment);
+        return "redirect:/comments";
+    }
 
 }
