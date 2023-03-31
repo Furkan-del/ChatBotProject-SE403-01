160e812c50d02f96dc48b222404e75e3aa6bd4a2
furkandel <57002551+Furkan-del@users.noreply.github.com>
2023-03-31 22:55:08 +0300

    links are added

---
+++ src/main/java/com/example/chatbot/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.chatbot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+
+
+}
